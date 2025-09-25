053a7005b9e454d21226da9c868a7670b63ed26f
Emilie <emer0005@stud.kea.dk>
2025-09-25 20:41:37 +0200

    js product implementeret

---
+++ product.js
@@ -0,0 +1,65 @@
+console.log("product site loaded ... ");
+
+const id = 1598;
+const productUrl = "https://kea-alt-del.dk/t7/api/products/" + id;
+const product_container = document.querySelector(".product_container");
+
+console.log("Product: ", productUrl);
+
+function getData() {
+  fetch(productUrl).then((res) => res.json().then((data) => show(data)));
+}
+
+function show(data) {
+  console.log("Shows data er: ", data);
+
+  product_container.innerHTML = `
+ <img class="product_img" src="https://kea-alt-del.dk/t7/images/webp/640/${id}.webp" alt="Grøn taske">
+
+        <div>
+            <section>
+                <h2>Product Information</h2>
+                <dl>
+                <div>
+                    <dt>Model name</dt>
+                    <dd>${data.productdisplayname}</dd>
+                 </div>
+                  <div>
+                    <dt>Gender</dt>
+                    <dd>${data.gender}</dd>
+                 </div>
+                  <div>
+                    <dt>Inventory number</dt>
+                    <dd>${data.id}</dd>
+                 </div>
+                  <div>
+                    <dt>Brand</dt>
+                    <dd>${data.brandname}</dd>
+                     </div>
+                </dl>
+
+                <p> DKK ${data.price},-</p>
+            </section>
+
+            <section>
+                <h3>${data.productdisplayname}</h3>
+                <p>
+                    <span> ${data.articletype} </span> | <span> ${data.brandname} </span>
+                </p>
+                <form>
+                    <label for="size">Choose a size:</label>
+                    <select name="size" id="size">
+                        <option value="s">S</option>
+                        <option value="m">M</option>
+                        <option value="l">L</option>
+                        <option value="xl">XL</option>
+                    </select>
+                    <button>Add to basket</button>
+                </form>
+
+            </section>
+        </div>
+   `;
+}
+
+getData();
